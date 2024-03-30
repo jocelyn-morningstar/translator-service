204f35a4949bf0789240f4a21bf5c2313a77be46
Jocelyn <jmorning@andrew.cmu.edu>
2024-03-30 17:45:20 -0400

    Testing openai and GCP connections

--- src/translator.py
+++ src/translator.py
@@ -1,16 +1,16 @@
 import os
-#from openai import OpenAI
-#client  = OpenAI(api_key = os.environ.get('OPENAI_API_KEY'))
+from openai import OpenAI
+client  = OpenAI(api_key = os.environ.get('OPENAI_API_KEY'))
 
 
 def translate_content(content: str) -> tuple[bool, str]:
-    #prompt = "Translate the following text into English: "
-    #completion = client.chat.completions.create(
-    #model = "gpt-3.5-turbo",
-    #messages = [
-     #   {"role": "user", "content": prompt + content}
-      #  ])
-    #llmresponse = completion.choices[0].message
-    #translation = llmresponse.content
-    translation = "something"
+    prompt = "Translate the following text into English: "
+    completion = client.chat.completions.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {"role": "user", "content": prompt + content}
+        ])
+    llmresponse = completion.choices[0].message
+    translation = llmresponse.content
+    #translation = "something"
     return True, translation
