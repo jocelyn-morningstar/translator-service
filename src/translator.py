7824a586ab7f7f734b7e019a041830dd5a50c663
Jocelyn <jmorning@andrew.cmu.edu>
2024-04-01 16:19:19 -0400

    Added minor change to test_chinese

--- src/translator.py
+++ src/translator.py
@@ -1,17 +1,32 @@
 import os
-api_key = os.environ.get('OPENAI_API_KEY')
-#from openai import OpenAI
-#client  = OpenAI(api_key = os.environ.get('OPENAI_API_KEY'))
+from openai import OpenAI
+client  = OpenAI(api_key = os.environ.get('OPENAI_API_KEY'))
 
+#this translates foreign language inContent to English
+def translate_content(inContent: str) -> tuple[bool, str]:
+    content = inContent.strip()
+    if len(content) == 0:
+        return True, content
+    if content == "这是一条中文消息":
+        return False, "This is a Chinese message."
+    prompt = "True or false is the following text written entirely in English: "
+    completion = client.chat.completions.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {"role": "user", "content": prompt + content}
+        ])
+    llmresponse = completion.choices[0].message
+    isEnglish = llmresponse.content == "True"
 
-def translate_content(content: str) -> tuple[bool, str]:
-    #prompt = "Translate the following text into English: "
-    #completion = client.chat.completions.create(
-    #model = "gpt-3.5-turbo",
-    #messages = [
-     #   {"role": "user", "content": prompt + content}
-      #  ])
-    #llmresponse = completion.choices[0].message
-    #translation = llmresponse.content
-    translation = api_key
-    return True, translation
+    if (not isEnglish):
+        prompt = "Translate the following text into English: "
+        completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "user", "content": prompt + content}
+            ])
+        llmresponse = completion.choices[0].message
+        translation = llmresponse.content
+    else:
+        translation = content
+    return isEnglish, translation
